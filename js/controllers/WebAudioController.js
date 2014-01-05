c37d8d90294f0a5b6ca45311484567ea06f5c9fb
Kev Adsett <kev.adsett@gmail.com>
2014-01-05 21:59:22 +0000

    Set Oscs as start parameters
    If no osc is specified, dummy osc is created with 0 gain.

--- js/controllers/WebAudioController.js
+++ js/controllers/WebAudioController.js
@@ -15,20 +15,28 @@ var WebAudioController = {
 }
 
 WebAudioController.Voice.prototype = {
-    start: function() {
-        var vco = WebAudioController.context.createOscillator();
-        vco.type = vco.SAWTOOTH;
-        vco.frequency.value = this.frequency;
-        
+    start: function(vco1, vco2) {
+        this.setupOsc(vco1);
+        this.setupOsc(vco2);
+    },
+    
+    setupOsc: function(vco) {
         var vca = WebAudioController.context.createGain();
-        vca.gain.value = 0.3;
         
+        if(vco) {
+            vca.gain.value = 0.3;
+        } else {
+            vco = WebAudioController.context.createOscillator();
+            vca.gain.value = 0;
+        }
+        vco.frequency.value = this.frequency;
         vco.connect(vca);
         vca.connect(WebAudioController.context.destination);
         
         vco.start(0);
         this.oscillators.push(vco);
     },
+    
     stop: function() {
         _.each(this.oscillators, function(osc) {
             osc.stop(0)
