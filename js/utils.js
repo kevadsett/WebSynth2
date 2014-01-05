1c75ae46891927c544004f831c02b87f34a43e96
Kev Adsett <kev.adsett@gmail.com>
2014-01-05 22:00:52 +0000

    Add angle conversion utils

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,11 @@ function mapValue(value, lo1, hi1, lo2, hi2){
 	var normal = normalise(value, lo1, hi1);
 	return normal * (hi2 - lo2) + lo2;
 }
+
+function degToRad(value) {
+    return value * (Math.PI / 180);
+}
+
+function radToDeg(value) {
+    return value * (180 / Math.PI);
+}
