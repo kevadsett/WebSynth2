4e9d172a87f1db460feb6bd51907c63a527ba92a
Kev Adsett <kev.adsett@gmail.com>
2014-01-13 14:40:43 +0000

    Add step and value code to pot model

--- js/models/PotModel.js
+++ js/models/PotModel.js
@@ -9,49 +9,74 @@ var PotModel = Backbone.Model.extend({
         console.log("initializing pot model");
         new PotView({model: this});
         this.on('change:angle', _.bind(this.limitAngle, this));
+        var values = this.get('values'),
+            steps = this.get('steps');
+        if(!values) {
+            if(steps) {
+                values = {
+                    lo: 0,
+                    hi: steps - 1
+                };
+            } else {
+                values = {
+                    lo: 0,
+                    hi: 1
+                }
+            }
+        }
+        this.set('values', values);
     },
     limitAngle: function() {
         var angle = (this.get('angle') + 360) % 360,
+            offsetAngle = this.getOffsetAngle(angle),
             loLimit = this.get('angleLimit').lo,
             hiLimit = this.get('angleLimit').hi,
-            steps = this.get('steps');
+            offsetHiLimit = this.getOffsetAngle(hiLimit),
+            steps = this.get('steps'),
+            values = this.get('values');
         
-        if(this.getOffsetAngle(angle) > this.getOffsetAngle(hiLimit)) {
-            console.log("Out of bounds");
-            if(Math.abs(angle - loLimit) <= Math.abs(angle - hiLimit)) {
-                angle = loLimit;
-            } else {
-                angle = hiLimit;
-            }
-        }
-        this.set('angle', angle);
-        /*
-        if(loLimit > hiLimit) {
-            hiLimit = hiLimit + 360;
-        }
-        console.log("lo: " + loLimit, "hi: " + hiLimit);
-        if(steps !== undefined) {
-            var angleBetween = hiLimit - loLimit,
-                singleStep = angleBetween / (steps - 1);
-            var closestAngle, closeness = 360, angleIndex = -1;
-            for(var i = 0; i < steps; i++) {
-                var targetAngle = (loLimit + (singleStep * i)) % 360;
-                if (Math.abs(angle - targetAngle) < closeness) {
-                    closeness = Math.abs(angle - targetAngle);
-                    closestAngle = targetAngle;
-                    angleIndex = i;
+        if(steps === undefined) {
+            if(offsetAngle > offsetHiLimit) {
+                if(Math.abs(angle - loLimit) <= Math.abs(angle - hiLimit)) {
+                    angle = loLimit;
+                } else {
+                    angle = hiLimit;
                 }
             }
-            angle = closestAngle;
+            this.set({
+                'angle': angle,
+                'value': mapValue(offsetAngle, 0, offsetHiLimit, values.lo, values.hi)
+            });
+        } else {
+            if(loLimit > hiLimit) {
+                hiLimit = hiLimit + 360;
+            }
+            if(steps !== undefined) {
+                var angleBetween = hiLimit - loLimit,
+                    singleStep = angleBetween / (steps - 1);
+                var closestAngle, closeness = 360, angleIndex = -1;
+                for(var i = 0; i < steps; i++) {
+                    var targetAngle = (loLimit + (singleStep * i)) % 360;
+                    if (Math.abs(angle - targetAngle) < closeness) {
+                        closeness = Math.abs(angle - targetAngle);
+                        closestAngle = targetAngle;
+                        angleIndex = i;
+                    }
+                }
+                angle = closestAngle;
+            }
+            
+            this.set({
+                'angle': angle,
+                'value': mapValue(angleIndex, 0, steps-1, values.lo, values.hi)
+            });
         }
-        
-        this.set({
-            'angle': angle,
-            'value': angleIndex
-        });
-        console.log(this.get('value'));*/
+        console.log(this.cid, toDecimalPlaces(this.get('value'), 2));
     },
     getOffsetAngle: function(angle) {
         return (angle - this.get('angleLimit').lo + 360) % 360;
+    },
+    getValue: function() {
+        return this.get('value');
     }
 });
