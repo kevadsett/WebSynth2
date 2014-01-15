96f5d64006ce245aaafb5cd1919ad5bb8c9cd8a6
Kev Adsett <kev.adsett@gmail.com>
2014-01-15 15:19:37 +0000

    Add notches to pot view

--- js/views/PotView.js
+++ js/views/PotView.js
@@ -27,14 +27,15 @@ var PotView = Backbone.View.extend({
             angle = Math.atan2(distY, distX);
         
         this.model.set('angle', radToDeg(angle));
-        
     },
+    
     withinBounds: function(coords) {
         var x = this.model.get('x'),
             y = this.model.get('y'),
             radius = this.model.get('radius');
         return coords.x > x - radius && coords.x < x + radius && coords.y > y - radius && coords.y < y + radius;
     },
+    
     render: function() {
         var ctx = this.context,
             centreX = this.model.get('x'),
@@ -42,6 +43,55 @@ var PotView = Backbone.View.extend({
             radius = this.model.get('radius');
         ctx.save();
         ctx.translate(centreX, centreY);
+        
+        // label
+        ctx.fillText(this.model.get('label'), 0 - radius, radius * 2);
+        
+        // notches
+        var angleLimit = this.model.get('angleLimit');
+        
+        if(this.model.get('steps')) {
+            var startAngle = this.model.get('startAngle');
+            _.each(this.model.get('stepValues'), function(stepAngle, index) {
+                var lineLength = 1.3;
+                if(stepAngle === startAngle && startAngle !== angleLimit.lo) {
+                    lineLength = 1.2;
+                } else {
+                    var isMiddleStep = (index < this.model.get('stepValues').length - 1) && index > 0;
+                    if(isMiddleStep) {
+                        lineLength = 1.1;
+                    }
+                }
+                ctx.save();
+                ctx.rotate(degToRad(stepAngle));
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo(radius * lineLength, 0);
+                ctx.stroke();
+                ctx.closePath();
+                ctx.restore();
+            }, this);
+        } else {
+            ctx.save();
+            ctx.rotate(degToRad(angleLimit.lo));
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(radius * 1.3, 0);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.restore();
+            
+            ctx.save();
+            ctx.rotate(degToRad(angleLimit.hi));
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(radius * 1.3, 0);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.restore();
+        }
+        
+        // pot
         ctx.rotate(degToRad(this.model.get('angle')));
         ctx.beginPath();
         ctx.arc(0, 0, radius, 0, 2 * Math.PI, false);
