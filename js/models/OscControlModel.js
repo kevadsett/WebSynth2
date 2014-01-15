2b50e003759187c5e602678de54570b19543c100
Kev Adsett <kev.adsett@gmail.com>
2014-01-15 15:18:11 +0000

    Add pitch control pots

--- js/models/OscControlModel.js
+++ js/models/OscControlModel.js
@@ -16,26 +16,60 @@ var OscControlModel = Backbone.Model.extend({
                     lo: 140,
                     hi: 40
                 },
-                angle: 140
+                startAngle: 140,
+                label: "Wave type"
             }),
             volumeFader: new FaderModel({
                 x: this.get('x') + 75,
                 y: this.get('y') - 25,
                 value: 1
             }),
-            detunePot: new PotModel({
+            octavePot: new PotModel({
                 x: this.get('x') + 150,
+                y: this.get('y'),
+                radius: 15,
+                steps:5,
+                angleLimit: {
+                    lo: 135, 
+                    hi: 45
+                },
+                startAngle: 270,
+                values: {
+                    lo:-2,
+                    hi:2
+                },
+                label: "Octave"
+            }),
+            pitchPot: new PotModel({
+                x: this.get('x') + 200,
+                y: this.get('y'),
+                radius: 15,
+                steps:25,
+                angleLimit: {
+                    lo: 135, 
+                    hi: 45
+                },
+                startAngle: 270,
+                values: {
+                    lo:-12,
+                    hi:12
+                },
+                label: "Pitch"
+            }),
+            detunePot: new PotModel({
+                x: this.get('x') + 250,
                 y: this.get('y'),
                 radius: 15,
                 angleLimit: {
                     lo: 135, 
                     hi: 45
                 },
-                angle: 270,
+                startAngle: 270,
                 values: {
                     lo:-100,
                     hi:100
-                }
+                },
+                label: "Detune"
             })
         });
         new OscControlView({model:this});
@@ -46,7 +80,13 @@ var OscControlModel = Backbone.Model.extend({
     getVolume: function() {
         return this.get('volumeFader').getValue();
     },
+    getOctaveOffset: function() {
+        return this.get('octavePot').getValue();
+    },
+    getPitchOffset: function() {
+        return this.get('pitchPot').getValue();
+    },
     getDetune: function() {
         return this.get('detunePot').getValue();
-    }
+    },
 });
